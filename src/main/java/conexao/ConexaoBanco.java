4655ff8dc18df13f8a4f8fcee604cd7d7f108a82
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2023-09-11 17:18:43 -0300

    V-0.0.5 Inicio do BackEnd - MVC - Servlet e JSP

---
+++ src/main/java/conexao/ConexaoBanco.java
@@ -0,0 +1,36 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexaoBanco {
+	private static String banco = "jdbc:mysql://localhost:3306/uc9_20221_4";
+	private static String usuario = "novousuario";
+	private static String senha = "1Q@w3e4r";
+	private static Connection conn = null;
+	
+	public static Connection getConnection(){
+		return conn;
+	}
+	
+	static {
+		conectar();
+	}
+	
+	public ConexaoBanco() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			if (conn == null) {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				conn = DriverManager.getConnection(banco, usuario, senha);
+				conn.setAutoCommit(false);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Usuario {
+	private Long id;
+	private String usuario;
+	private String senha;
+}
